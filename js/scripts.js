6ea516349037ee2f72ae9e9fc6f77b1740cca9f0
Kamil Mangold <kamilmangold@gmail.com>
2017-07-10 20:12:20 +0200

    ostateczne

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-    $.getJSON(quoteUrl, createTweet);
+    $.getJSON(prefix + quoteUrl, createTweet);
+    $.ajaxSetup({ cache: false });
 }
 
 function createTweet(input) {
@@ -17,13 +19,12 @@ function createTweet(input) {
     var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
     if (tweetText.length > 140) {
     	getQuote();
-		else {
-    		var tweet = tweetLink + encodeURIComponent(tweetText);
-		    $('.quote').text(quoteText);
-		    $('.author').text("Author: " + quoteAuthor);
-		    $('.tweet').attr('href', tweet);
-		}
-}
+	} else {
+		var tweet = tweetLink + encodeURIComponent(tweetText);
+	    $('.quote').text(quoteText);
+	    $('.author').text("Author: " + quoteAuthor);
+	    $('.tweet').attr('href', tweet);
+	}
 }
 $(document).ready(function() {
     getQuote();
